ca84acec6262e613dbef5032d189b59ff86f10ed
Vitor Minhoto <vitor.minhoto@inesctec.pt>
2025-09-26 19:09:32 +0100

    1st test on saving a user to the database. Check if connection is working and if repository is being properly injected and queries autogenerated

--- app/src/main/kotlin/com/vminhoto/chirp/ChirpApplication.kt
+++ app/src/main/kotlin/com/vminhoto/chirp/ChirpApplication.kt
@@ -1,13 +1,32 @@
 package com.vminhoto.chirp
 
-import com.vminhoto.chat.Test
+import com.vminhoto.chirp.infra.database.entities.UserEntity
+import com.vminhoto.chirp.infra.database.repositories.UserRepository
+import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Component
 
 @SpringBootApplication
 class ChirpApplication
 
 fun main(args: Array<String>) {
-    Test()
 	runApplication<ChirpApplication>(*args)
+}
+
+@Component
+class Demo(
+    private val repository: UserRepository
+) {
+
+    @PostConstruct
+    fun init() {
+        repository.save(
+            UserEntity(
+                email = "test@test.com",
+                username = "test",
+                hashedPassword = "123"
+            )
+        )
+    }
 }
