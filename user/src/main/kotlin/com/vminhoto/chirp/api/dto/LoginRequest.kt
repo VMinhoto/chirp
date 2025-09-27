6332a96536b7dcdfa303d1327bf87db7461229b6
Vitor <up201303086@fe.up.pt>
2025-09-27 03:45:27 +0100

    Changed the login request DTO to be compatible with jackson

--- user/src/main/kotlin/com/vminhoto/chirp/api/dto/LoginRequest.kt
+++ user/src/main/kotlin/com/vminhoto/chirp/api/dto/LoginRequest.kt
@@ -1,7 +1,11 @@
 package com.vminhoto.chirp.api.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 // Corresponds to data coming from a login request.
 data class LoginRequest(
+    @JsonProperty("email")
     val email: String,
+    @JsonProperty("password")
     val password: String
 )
