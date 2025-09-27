52dc042c2aefc21cde905b32caaa40d47e027dde
Vitor <up201303086@fe.up.pt>
2025-09-27 03:46:30 +0100

    Implemented Login function in the controller for the login endpoint

--- user/src/main/kotlin/com/vminhoto/chirp/api/controllers/AuthController.kt
+++ user/src/main/kotlin/com/vminhoto/chirp/api/controllers/AuthController.kt
@@ -1,7 +1,10 @@
 package com.vminhoto.chirp.api.controllers
 
+import com.vminhoto.chirp.api.dto.AuthenticatedUserDto
+import com.vminhoto.chirp.api.dto.LoginRequest
 import com.vminhoto.chirp.api.dto.RegisterRequest
 import com.vminhoto.chirp.api.dto.UserDto
+import com.vminhoto.chirp.api.mappers.toAuthenticatedUserDto
 import com.vminhoto.chirp.api.mappers.toUserDto
 import com.vminhoto.chirp.service.auth.AuthService
 import jakarta.validation.Valid
@@ -23,5 +26,15 @@ class AuthController(private val authService: AuthService) {
             username = body.username,
             password = body.password
         ).toUserDto()
+    }
+
+    @PostMapping("/login")
+    fun login(
+        @RequestBody body: LoginRequest
+    ) : AuthenticatedUserDto {
+        return authService.login(
+            email = body.email,
+            password = body.password
+        ).toAuthenticatedUserDto()
     }
 }
