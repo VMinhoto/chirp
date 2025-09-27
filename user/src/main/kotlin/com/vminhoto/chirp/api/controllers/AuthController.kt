73b5859de9bf62f38e97b631a8c7c9b2740940ef
Vitor <up201303086@fe.up.pt>
2025-09-27 04:13:00 +0100

    Refresh function on controller for the refresh endpoint

--- user/src/main/kotlin/com/vminhoto/chirp/api/controllers/AuthController.kt
+++ user/src/main/kotlin/com/vminhoto/chirp/api/controllers/AuthController.kt
@@ -2,6 +2,7 @@ package com.vminhoto.chirp.api.controllers
 
 import com.vminhoto.chirp.api.dto.AuthenticatedUserDto
 import com.vminhoto.chirp.api.dto.LoginRequest
+import com.vminhoto.chirp.api.dto.RefreshRequest
 import com.vminhoto.chirp.api.dto.RegisterRequest
 import com.vminhoto.chirp.api.dto.UserDto
 import com.vminhoto.chirp.api.mappers.toAuthenticatedUserDto
@@ -36,5 +37,14 @@ class AuthController(private val authService: AuthService) {
             email = body.email,
             password = body.password
         ).toAuthenticatedUserDto()
+    }
+
+    @PostMapping("/refresh")
+    fun refresh(
+        @RequestBody body: RefreshRequest
+    ): AuthenticatedUserDto {
+        return authService
+            .refresh(body.refreshToken)
+            .toAuthenticatedUserDto()
     }
 }
