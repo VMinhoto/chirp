2e05ff3c59f6f423f548f27d8bb37a08935b540d
Vitor Minhoto <vitor.minhoto@inesctec.pt>
2025-10-14 20:30:37 +0100

    Called change password from the change password endpoint

--- user/src/main/kotlin/com/vminhoto/chirp/api/controllers/AuthController.kt
+++ user/src/main/kotlin/com/vminhoto/chirp/api/controllers/AuthController.kt
@@ -11,11 +11,14 @@ import com.vminhoto.chirp.api.dto.ResetPasswordRequest
 import com.vminhoto.chirp.api.dto.UserDto
 import com.vminhoto.chirp.api.mappers.toAuthenticatedUserDto
 import com.vminhoto.chirp.api.mappers.toUserDto
+import com.vminhoto.chirp.api.util.requestUserId
+import com.vminhoto.chirp.domain.model.UserId
 import com.vminhoto.chirp.infra.rate_limiting.EmailRateLimiter
 import com.vminhoto.chirp.service.AuthService
 import com.vminhoto.chirp.service.EmailVerificationService
 import com.vminhoto.chirp.service.PasswordResetService
 import jakarta.validation.Valid
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -128,6 +131,10 @@ class AuthController(
     fun changePassword(
         @Valid @RequestBody body: ChangePasswordRequest
     ){
-        // TODO: Extract request user ID and call service
+        passwordResetService.changePassword(
+            userId = requestUserId,
+            oldPassword = body.oldPassword,
+            newPassword = body.newPassword
+        )
     }
 }
