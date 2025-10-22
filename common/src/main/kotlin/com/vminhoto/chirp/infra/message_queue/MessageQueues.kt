42202d410803f6f4c717d6530aca904373f4792b
Vitor Minhoto <vitor.minhoto@inesctec.pt>
2025-10-22 19:10:19 +0100

    Define RabbitMQ config beans and message name constants

---
+++ common/src/main/kotlin/com/vminhoto/chirp/infra/message_queue/MessageQueues.kt
@@ -0,0 +1,8 @@
+package com.vminhoto.chirp.infra.message_queue
+
+/**
+ * Object that holds all keys representing the queues events are sent to
+ */
+object MessageQueues {
+    const val NOTIFICATION_USER_EVENTS = "notification_user_events"
+}
