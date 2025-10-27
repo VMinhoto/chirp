09dcfe6ea99078ece4c62fff2baa3bfc161e8055
Vitor Minhoto <vitor.minhoto@inesctec.pt>
2025-10-27 17:27:23 +0000

    Add and delete user from chat endpoint implementation

--- chat/src/main/kotlin/com/vminhoto/chirp/api/controllers/ChatController.kt
+++ chat/src/main/kotlin/com/vminhoto/chirp/api/controllers/ChatController.kt
@@ -1,11 +1,15 @@
 package com.vminhoto.chirp.api.controllers
 
+import com.vminhoto.chirp.api.dto.AddParticipantToChatDto
 import com.vminhoto.chirp.api.dto.ChatDto
 import com.vminhoto.chirp.api.dto.CreateChatRequest
 import com.vminhoto.chirp.api.mappers.toChatDto
 import com.vminhoto.chirp.api.util.requestUserId
+import com.vminhoto.chirp.domain.type.ChatId
 import com.vminhoto.chirp.service.ChatService
 import jakarta.validation.Valid
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -32,5 +36,37 @@ class ChatController(
             creatorId = requestUserId,
             otherUserIds = body.otherUsersIds.toSet()
         ).toChatDto()
+    }
+
+    /**
+     * Endpoint to add a user to a chat.
+     * @param chatId The Id of the chat. Comes from the path of the request
+     * @param body body of the request where it contains the info of the participant to add.
+     * @return A chat DTO representing the updated chat.
+     */
+    @PostMapping("/{chatId}/add")
+    fun addChatParticipants(
+        @PathVariable chatId: ChatId,
+        @Valid @RequestBody body: AddParticipantToChatDto
+    ): ChatDto {
+        return chatService.addParticipantsToChat(
+            requestUserId = requestUserId,
+            chatId = chatId,
+            userIds = body.userIds.toSet()
+        ).toChatDto()
+    }
+
+    /**
+     * Endpoint to leave a chat.
+     * @param chatId The Id of the chat. Comes from the path of the request
+     */
+    @DeleteMapping("/{chatId}/leave")
+    fun leaveChat(
+        @PathVariable chatId: ChatId,
+    ) {
+        chatService.removeParticipantFromChat(
+            chatId = chatId,
+            userId = requestUserId
+        )
     }
 }
