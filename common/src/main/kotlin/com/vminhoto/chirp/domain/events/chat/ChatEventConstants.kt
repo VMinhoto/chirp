cf8cf03b439443a66e26a474d524a0557daefffb
Vitor <up201303086@fe.up.pt>
2025-10-27 02:25:01 +0000

    Constant to defime the key for the RABBITMQ chat.

---
+++ common/src/main/kotlin/com/vminhoto/chirp/domain/events/chat/ChatEventConstants.kt
@@ -0,0 +1,8 @@
+package com.vminhoto.chirp.domain.events.chat
+
+/**
+ * Object to define event keys on RabbitMQ for chat events.
+ */
+object ChatEventConstants {
+    const val CHAT_EXCHANGE = "chat.events"
+}
