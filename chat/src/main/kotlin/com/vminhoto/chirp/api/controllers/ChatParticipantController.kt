e5ca025678aa31d67861fc9d268345a694e7f275
Vitor <up201303086@fe.up.pt>
2025-10-29 01:06:29 +0000

    Created endpoints for upload, confirm and delete profile picture.

--- chat/src/main/kotlin/com/vminhoto/chirp/api/controllers/ChatParticipantController.kt
+++ chat/src/main/kotlin/com/vminhoto/chirp/api/controllers/ChatParticipantController.kt
@@ -1,11 +1,19 @@
 package com.vminhoto.chirp.api.controllers
 
 import com.vminhoto.chirp.api.dto.ChatParticipantDto
+import com.vminhoto.chirp.api.dto.ConfirmProfilePictureRequest
+import com.vminhoto.chirp.api.dto.PictureUploadResponse
 import com.vminhoto.chirp.api.mappers.toChatParticipantDto
+import com.vminhoto.chirp.api.mappers.toResponse
 import com.vminhoto.chirp.api.util.requestUserId
 import com.vminhoto.chirp.service.ChatParticipantService
+import com.vminhoto.chirp.service.ProfilePictureService
+import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -16,8 +24,11 @@ import org.springframework.web.server.ResponseStatusException
  * @param chatParticipantService chat participant service.
  */
 @RestController
-@RequestMapping("/api/chat/participants")
-class ChatParticipantController(private val chatParticipantService: ChatParticipantService) {
+@RequestMapping("/api/participants")
+class ChatParticipantController(
+    private val chatParticipantService: ChatParticipantService,
+    private val profilePictureService: ProfilePictureService
+) {
 
     /**
      * Endpoint to get a Chat participant by username or email. If no query is passed use the current logged user
@@ -38,5 +49,44 @@ class ChatParticipantController(private val chatParticipantService: ChatParticip
 
         return participant?.toChatParticipantDto()
             ?: throw ResponseStatusException(HttpStatus.NOT_FOUND)
+    }
+
+    /**
+     * Endpoint upload a picture. It generates the credentials for the client to upload it to supabase.
+     * @param mimeType The type of media.
+     * @return Picture upload response with info to upload the picture.
+     */
+    @PostMapping("/profile-picture-upload")
+    fun getProfilePictureUploadUrl(
+        @RequestParam mimeType: String
+    ): PictureUploadResponse {
+        return profilePictureService.generateUploadCredentials(
+            userId = requestUserId,
+            mimeType = mimeType
+        ).toResponse()
+    }
+
+    /**
+     * Endpoint when the client confirms the upload of the picture
+     * @param body with the publicURL
+     */
+    @PostMapping("/confirm-profile-picture")
+    fun confirmProfilePictureUploadUrl(
+        @Valid @RequestBody body: ConfirmProfilePictureRequest
+    ) {
+        return profilePictureService.confirmProfilePictureUpload(
+            userId = requestUserId,
+            publicUrl = body.publicUrl
+        )
+    }
+
+    /**
+     * Endpoint when the user deletes the profile picture.
+     */
+    @DeleteMapping("/profile-picture")
+    fun deleteProfilePicture() {
+        profilePictureService.deleteProfilePicture(
+            userId = requestUserId
+        )
     }
 }
