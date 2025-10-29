54a0aff9698eaef51b722a312c578ac16badc4e7
Vitor <up201303086@fe.up.pt>
2025-10-29 01:05:59 +0000

    Implement exception handlers

--- chat/src/main/kotlin/com/vminhoto/chirp/api/exception_handling/ChatExceptionHandler.kt
+++ chat/src/main/kotlin/com/vminhoto/chirp/api/exception_handling/ChatExceptionHandler.kt
@@ -3,7 +3,9 @@ package com.vminhoto.chirp.api.exception_handling
 import com.vminhoto.chirp.domain.exception.ChatNotFoundException
 import com.vminhoto.chirp.domain.exception.ChatParticipantNotFoundException
 import com.vminhoto.chirp.domain.exception.InvalidChatSizeException
+import com.vminhoto.chirp.domain.exception.InvalidProfilePictureException
 import com.vminhoto.chirp.domain.exception.MessageNotFoundException
+import com.vminhoto.chirp.domain.exception.StorageException
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -30,6 +32,7 @@ class ChatExceptionHandler {
         "code" to "NOT_FOUND",
         "message" to e.message
     )
+
     /**
      * Handle [InvalidChatSizeException]. Returns Bad Request INVALID_CHAT_SIZE. With the message from the exception.
      * @param e Exception
@@ -38,6 +41,29 @@ class ChatExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     fun onForbidden(e: InvalidChatSizeException) = mapOf(
         "code" to "INVALID_CHAT_SIZE",
+        "message" to e.message
+    )
+
+    /**
+     * Handle [InvalidProfilePictureException]. Returns Bad Request INVALID_PROFILE_PICTURE. With the message from
+     * the exception.
+     * @param e Exception
+     */
+    @ExceptionHandler(InvalidProfilePictureException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun onInvalidProfilePicture(e: InvalidProfilePictureException) = mapOf(
+        "code" to "INVALID_PROFILE_PICTURE",
+        "message" to e.message
+    )
+
+    /**
+     * Handle [StorageException]. Returns Bad Request STORAGE_ERROR. With the message from the exception.
+     * @param e Exception
+     */
+    @ExceptionHandler(StorageException::class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    fun onInvalidProfilePicture(e: StorageException) = mapOf(
+        "code" to "STORAGE_ERROR",
         "message" to e.message
     )
 
