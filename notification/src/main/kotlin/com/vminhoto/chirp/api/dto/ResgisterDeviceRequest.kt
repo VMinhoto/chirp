79db216bfa84cb5a53c7e8c866c1b6a1c8a41148
Vitor <up201303086@fe.up.pt>
2025-10-30 18:49:24 +0000

    Fixed Request class to properly parse the json

--- notification/src/main/kotlin/com/vminhoto/chirp/api/dto/ResgisterDeviceRequest.kt
+++ notification/src/main/kotlin/com/vminhoto/chirp/api/dto/ResgisterDeviceRequest.kt
@@ -1,10 +1,13 @@
 package com.vminhoto.chirp.api.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import jakarta.validation.constraints.NotBlank
 
 data class RegisterDeviceRequest(
     @field:NotBlank
+    @JsonProperty("token")
     val token: String,
+    @JsonProperty(value = "platform")
     val platform: PlatformDto,
 )
 
